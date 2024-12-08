3452df85bf063188598ec4febaa9b41e862d0e3c
MartinLuxury <166442590+MartinLuxury@users.noreply.github.com>
2024-12-08 23:59:01 +0100

    conexion db prod

--- database/config.js
+++ database/config.js
@@ -1,12 +1,24 @@
 
 const mongoose = require('mongoose');
 
+// const dbConnection = async () => {
+//     try {
+//         await mongoose.connect("mongodb+srv://admin_imbau:yBeawTJc2oxjUrf9@imbau.yy0hgxm.mongodb.net/imbaudb", {
+//             socketTimeoutMS: 45000, // opción de timeout
+//             family: 4  // forzar el uso de IPv4
+//         });
+
+//         console.log('Connected');
+//     } catch (error) {
+//         console.error(error);
+//         throw new Error('Error en la connection: ' + error.message);
+//     }
+// }
+
+//para prodcucio
 const dbConnection = async () => {
     try {
-        await mongoose.connect("mongodb+srv://admin_imbau:yBeawTJc2oxjUrf9@imbau.yy0hgxm.mongodb.net/imbaudb", {
-            socketTimeoutMS: 45000, // opción de timeout
-            family: 4  // forzar el uso de IPv4
-        });
+        await mongoose.connect(process.env.MONGODB_URL_ATLAS);
 
         console.log('Connected');
     } catch (error) {
